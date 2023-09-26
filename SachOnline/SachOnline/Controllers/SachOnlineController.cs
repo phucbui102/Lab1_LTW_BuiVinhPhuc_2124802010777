840d0c778b98213cb0bbc5259e85866cd93d239a
phucbui <laptaolao@gmail.com>
2023-09-26 07:43:12 +0700

    update code

--- SachOnline/SachOnline/Controllers/SachOnlineController.cs
+++ SachOnline/SachOnline/Controllers/SachOnlineController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using SachOnline.Models;
 namespace SachOnline.Controllers
 {
     public class SachOnlineController : Controller
     {
         // GET: SachOnline
+        dbSachOnlineDataContext data = new dbSachOnlineDataContext();
+
+        private List<SACH>LaySachMoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var listSachMoi = LaySachMoi(6);
+
+            return View(listSachMoi);
+
+        }
+
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = from cd in data.CHUDEs select cd;
+            return PartialView(listChuDe);
+        }
+        public ActionResult NhaXuatBanPartial()
+        {
+            var listNhaXuatBan = from cd in data.NhaXuatBans select cd;
+            return PartialView(listNhaXuatBan);
         }
     }
 }

a7f62f3db38abfa7d960e851155dc70fa27821d2
phucbui <laptaolao@gmail.com>
2023-10-03 08:50:15 +0700

    update code for lab 03

--- SachOnline/SachOnline/Controllers/UserController.cs
+++ SachOnline/SachOnline/Controllers/UserController.cs
@@ -9,12 +9,119 @@ namespace SachOnline.Controllers
     public class UserController : Controller
     {
         // GET: User
+
+        dbSachOnlineDataContext db = new dbSachOnlineDataContext();
         public ActionResult Index()
         {
             return View();
         }
-        [HttpPost]
-        public ActionResult DangKy(FormCollection collection, KhachHang kh)
+        [HttpGet]
+        public ActionResult Dangky()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Dangky(FormCollection collection, KhachHang kh)
+        {
+
+            var sHoTen = collection["HoTen"];
+            var sTenDN = collection["TenDN"];
+            var sMatKhau = collection["MatKhau"];
+            var sMatKhauNhapLai = collection["MatKhauNL"];
+            var sDiaChi = collection["DiaChi"];
+            var sEmail = collection["Email"];
+            var sDienThoai = collection["DienThoai"];
+            var dNgaySinh = String.Format("{0:MM/dd/yyyy}", collection["NgaySinh"]);
+            if (String.IsNullOrEmpty(sHoTen))
+            {
+                ViewData["err1"] = "Họ tên không được rỗng";
+            }
+            else if (String.IsNullOrEmpty(sTenDN))
+            {
+                ViewData["err2"] = "Tên đăng nhập không được rỗng";
+            }
+            else if (String.IsNullOrEmpty(sMatKhau))
+            {
+                ViewData["err3"] = "Phải nhập mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(sMatKhauNhapLai))
+            {
+                ViewData["err4"] = "Phải nhập lại mật khẩu";
+            }
+            else if (sMatKhau != sMatKhauNhapLai)
+            {
+                ViewData["err4"] = "Mật khẩu nhập lại không khớp";
+            }
+            else if (String.IsNullOrEmpty(sEmail))
+            {
+                ViewData["err5"] = "Email không được rỗng";
+            }
+            else if (String.IsNullOrEmpty(sDienThoai))
+            {
+                ViewData["err6"] = "Số điện thoại không được rỗng";
+            }
+            else if (db.KhachHangs.SingleOrDefault(n => n.TaiKhoan == sTenDN) != null)
+            {
+                ViewBag.ThongBao = "Tên đăng nhật đã tồn tại";
+            }
+            else if (db.KhachHangs.SingleOrDefault(n => n.Email == sEmail) != null)
+            {
+                ViewBag.ThongBao = "Email đã được sử dụng";
+            }
+            else
+            {
+                kh.HoTen = sHoTen;
+                kh.TaiKhoan = sTenDN;
+                kh.Email = sEmail;
+                kh.DiaChi = sDiaChi;
+                kh.DienThoai = sDienThoai;
+                kh.NgaySinh = DateTime.Parse(dNgaySinh);
+                db.KhachHangs.InsertOnSubmit(kh);
+                db.SubmitChanges();
+                return RedirectToAction("DangNhap");
+            }
+
+            return this.Dangky();
+        }
+
+
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            var sTenDN = collection["TenDN"];
+            var sMatKhau = collection["MatKhau"];
+            if (String.IsNullOrEmpty(sTenDN))
+            {
+                ViewData["err1"] = "Bạn chưa nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(sMatKhau))
+            {
+                ViewData["err2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                KhachHang kh = db.KhachHangs.SingleOrDefault(n => n.TaiKhoan == sTenDN && n.MatKhau == sMatKhau);
+                if (kh != null)
+                {
+                    ViewBag.ThongBao = "Chúc mừng đăng nhập thành công";
+                    Session["TaiKhoan"] = kh;
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+            }
+            return View();
+        }
+
+    }
+}
